336af6c53677641529178f11211142b149259bfb
BichevoyIgor <igoreek@mail.ru>
2022-02-07 21:14:34 +0300

    hw5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import org.hibernate.SessionFactory;
+
 public class Main {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = GetSessionFactory.getSessionFactory();
+        StudentDao studentDao = new StudentDao(sessionFactory);
+        System.out.println(studentDao.findAllStudents());
+
+
+    }
 }
